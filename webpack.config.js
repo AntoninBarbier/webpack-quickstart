75e6794cce5ee2a4bbd1266990f5cb3370d6936d
AntoninBarbier <skery.antonin@outlook.com>
2021-04-23 00:46:01 +0200

    Add babel

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+let mode = process.env.NODE_ENV === "production" ? "production" : "development"
+
+module.exports = {
+    mode: mode,
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    devServer: {
+        contentBase: "./dist/",
+    }
+}
